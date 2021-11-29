cbec66154ed23e93b870247a467639a7617175ab
Breno Lomasso <brenomlomasso@gmail.com>
2021-11-29 10:53:33 -0300

    WIP fixing paths and bugs

--- breno-csv-converter/converter.py
+++ breno-csv-converter/converter.py
@@ -7,8 +7,8 @@ logging.basicConfig(
 )
 
 logger = logging.getLogger(__name__)
-input_path = Path("./input/")
-output_path = Path("./output/")
+input_path = Path("input/")
+output_path = Path("output/")
 
 
 @click.command()
@@ -35,23 +35,25 @@ def convert_files(source: Path, delimiter: str = ","):
     logger.info("Reading all files from the path %s:", source)
     data = list()
     for file in source.iterdir():
+        logger.info("Reading file: %s", file.name)
+
         if file.suffix == ".json":
-            data = read_json_file(source)
+            data = read_json_file(file)
             save_csv_file(data, file.name)
         elif file.suffix == ".csv":
-            data = read_csv_file(source, delimiter)
+            data = read_csv_file(file, delimiter)
             save_json_file(data, file.name)
 
 
-def read_csv_file(source: Path, delimiter: str) -> tuple:
-    with open(source, "r") as file:
-        rows = [row.split(delimiter) for row in file]
-        csv_data = dict.fromkeys(rows[0], [])
+def read_csv_file(file: Path, delimiter: str) -> tuple:
+    with open(file, "r") as file:
+        rows = [row.rstrip('\n').split(delimiter) for row in file]
+        csv_data = {key: list() for key in rows[0]}
         data_rows = rows[1:]
 
         for data in data_rows:
             for i, field in enumerate(csv_data.keys()):
-                csv_data[field].append(data[i].strip)
+                csv_data[field].append(data[i].strip())
 
     return csv_data
 
@@ -83,24 +85,23 @@ def save_csv_file(jsons: list, file_name: str):
     logger.info("bla bla")
 
 
-"""TODO: change csv_data to dict"""
-def save_json_file(csv_data: dict, file_name: str):
-    for field, datas in csv_data.items():
+def save_json_file(csv_data_dict: dict, file_name: str):
+    for field, csv_data in csv_data_dict.items():
         new_file_name = output_path.joinpath(file_name.split(".")[0] + ".json")
         logger.info("Saving json file: %s", new_file_name)
 
         with open(new_file_name, "w") as file:
             file.write("[\n")
 
-            for data in datas:
+            for data in csv_data:
                 tab = "".ljust(4, " ")
                 begin = "{\n"
                 file.write(f"{tab}{begin}")
 
-                for row in rows:
-                    file.write(format_json(row, (row != rows[-1])))
+                for row in data:
+                    file.write(format_json(row, (row != data[-1])))
 
-                if rows != data[-1]:
+                if data != csv_data[-1]:
                     end = "},\n"
                 else:
                     end = "}\n"
